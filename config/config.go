789a31a7893e991623422840b94d4d191388da31
Asseken <72454306+Asseken@users.noreply.github.com>
2024-01-17 10:43:49 +0800

    Add files via upload

---
+++ config/config.go
@@ -0,0 +1,77 @@
+package config
+
+import (
+	_ "embed"
+	"fmt"
+	"os"
+	"strings"
+)
+
+//go:embed version
+var version string
+
+//go:embed name
+var name string
+
+type LogLevel string
+
+const (
+	Debug  LogLevel = "debug"
+	Info   LogLevel = "info"
+	Notice LogLevel = "notice"
+	Warn   LogLevel = "warn"
+	Error  LogLevel = "error"
+)
+
+func GetVersion() string {
+	return strings.TrimSpace(version)
+}
+
+func GetName() string {
+	return strings.TrimSpace(name)
+}
+
+func GetLogLevel() LogLevel {
+	if IsDebug() {
+		return Debug
+	}
+	logLevel := os.Getenv("XUI_LOG_LEVEL")
+	if logLevel == "" {
+		return Info
+	}
+	return LogLevel(logLevel)
+}
+
+func IsDebug() bool {
+	return os.Getenv("XUI_DEBUG") == "true"
+}
+
+func GetBinFolderPath() string {
+	binFolderPath := os.Getenv("XUI_BIN_FOLDER")
+	if binFolderPath == "" {
+		//binFolderPath = "bin"
+		binFolderPath = "/etc" //修改db数据库存放位置
+	}
+	return binFolderPath
+}
+
+func GetDBFolderPath() string {
+	dbFolderPath := os.Getenv("XUI_DB_FOLDER")
+	if dbFolderPath == "" {
+		dbFolderPath = "/etc/x-ui"
+	}
+	return dbFolderPath
+}
+
+func GetDBPath() string {
+	return fmt.Sprintf("%s/%s.db", GetDBFolderPath(), GetName())
+}
+
+func GetLogFolder() string {
+	logFolderPath := os.Getenv("XUI_LOG_FOLDER")
+	if logFolderPath == "" {
+		//logFolderPath = "/var/log"
+		logFolderPath = "/etc/log" //修改日志存放位置
+	}
+	return logFolderPath
+}
